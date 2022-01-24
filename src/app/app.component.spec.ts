4031e749ded8b5f4a67f7388f9d9fa333f3a912d
Ebert <ebertpa@gmail.com>
2022-01-24 18:06:35 +0100

    Inicio proyecto angular my-store

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-store app is running!');
+    // expect(compiled.querySelector('.content span')?.textContent).toContain('my-store app is running!');
+    // expect(compiled.querySelector('span')?.textContent).toContain('my-store app is running!');
+    expect(compiled.querySelector('router-outlet')?.textContent).toContain('');
   });
 });
